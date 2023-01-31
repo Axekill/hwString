5ac5f3d4a276ee522eacfac13838b53a8e766aba
Axekill <andrei3.ru@ya.ru>
2023-01-31 16:09:49 +0300

    the end

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,34 @@
 public class Main {
+    public static void main(String[] args) {
+        task1();
+        System.out.println();
+        task2();
+        System.out.println();
+        task3();
+    }
 
+    private static void task1() {
+        String firstName = " Ivan ";
+        String middleName = " Ivanovich";
+        String lastName = "Ivanov ";
+        String fullName = lastName + firstName + middleName;
+        System.out.println("ФИО сотрудника — "+fullName);
+    }
+
+    private static void task2() {
+        String firstName = " Ivan ";
+        String middleName = " Ivanovich";
+        String lastName = "Ivanov ";
+        String fullName = lastName + firstName + middleName;
+        System.out.println("Данные ФИО сотрудника для заполнения отчета — "+fullName.toUpperCase());
+    }
+
+    private static void task3() {
+        String firstName = " Семён ";
+        String middleName = " Семёнович";
+        String lastName = "Иванов ";
+        String fullName = lastName + firstName + middleName;
+        String fullNameTrue = fullName.replace("ё", "е");
+        System.out.println("Данные ФИО сотрудника —  "+fullNameTrue);
+    }
 }
